2b41f0c6690e57d902af42fed81cd886c56f58c8
RyanP <rpalmber@ucsc.edu>
2021-07-15 19:10:54 -0700

    StartofPaintDecay

--- src/scenes/PaintingDark.js
+++ src/scenes/PaintingDark.js
@@ -13,14 +13,45 @@ class PaintingDarkness extends Phaser.Scene {
 
     create () {
         keyC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.C);
-        this.add.image(0, 0, 'paintingDark').setOrigin(0);
+        this.paint = this.add.image(0, 0, 'paintingDark').setOrigin(0);
         this.add.text(centerX, game.config.height - 20, 'Press C to stop looking at the picture?').setOrigin(0.5);
+        this.creep = 0;
+        let creepConfig = {
+            fontFamily: 'Georgia',
+            fontSize: '35px',
+            color: '#F5F5DC',
+            padding: {
+              top: 5,
+              bottom: 5,
+            },
+            fixedWidth: 100
+          }
+        this.creepLeft = this.add.text(100, 100, this.creep, creepConfig);
     }
 
     update() {
         if (Phaser.Input.Keyboard.JustDown(keyC)) {
             this.scene.stop();
             this.scene.resume('playScene');
+        }
+        this.creep++;
+        this.displayCreep = Math.floor(this.creep);
+        this.creepLeft.text = this.displayCreep;
+        if (this.creep == 300) {
+            this.paint = this.add.image(0, 0, 'paintingDark2').setOrigin(0);
+            this.creepLeft.text = this.displayCreep;
+        if (this.creep == 600) {
+            this.add.image(0, 0, 'paintingDark3').setOrigin(0);
+            this.creepLeft.text = this.displayCreep;
+        }
+        if (this.creep == 900) {
+            this.add.image(0, 0, 'paintingDark4').setOrigin(0);
+            this.creepLeft.text = this.displayCreep;
+        }
+        if (this.creep == 1200) {
+            this.add.image(0, 0, 'paintingDark5').setOrigin(0);
+            this.creepLeft.text = this.displayCreep;
+        }
         }
     }
     
