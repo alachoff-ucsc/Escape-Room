59f8acb7c12fd39c35daf4f81b7d9d48ac2a9d6b
RyanP <rpalmber@ucsc.edu>
2021-07-22 00:28:34 -0700

    Credits

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -16,9 +16,21 @@ class Menu extends Phaser.Scene {
       },
       fixedWidth: 0   
     }
+    let titleConfig = {
+      fontFamily: 'Georgia',
+      fontSize: '24px',
+      backgroundColor: '#800000',
+      color: '#FFFFFF',
+      align: 'right',
+      padding: {
+        top: 5,
+        bottom: 5,
+      },
+      fixedWidth: 0   
+    }
 
     // show menu text
-    this.add.text(game.config.width/2, game.config.height/5, 'The Face in the Dark', menuConfig).setOrigin(0.5);
+    this.add.text(game.config.width/2, game.config.height/5, 'The Face in the Dark', titleConfig).setOrigin(0.5);
     this.add.text(game.config.width/2, game.config.height/3.5, 'Use WASD to move', menuConfig).setOrigin(0.5);
     this.add.text(game.config.width/2, game.config.height/2.5, 'Objective: Use your wits to escape before time runs out!', menuConfig).setOrigin(0.5);
     this.add.text(game.config.width/2, game.config.height/2, 'Use E to interact with objects', menuConfig).setOrigin(0.5);
